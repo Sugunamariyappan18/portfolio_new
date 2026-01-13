e934e5855a1aaa30bdd162f41d0b9da14541b1c9
Sugunamariyappan18 <130042740+Sugunamariyappan18@users.noreply.github.com>
2026-01-13 15:18:18 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC = () => {
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-400 opacity-75"></span>
             <span className="relative inline-flex rounded-full h-2 w-2 bg-indigo-500"></span>
           </span>
-          <span>Available for new </span>
+          <span>Available for new opportunities</span>
         </div>
         
         <h1 className="text-4xl md:text-7xl font-bold mb-6 tracking-tight">
